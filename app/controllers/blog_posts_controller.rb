1aab6b8c3e71f71949c90673e2df63b38c4546e9
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-09-09 20:43:10 +0700

    init: update blog

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -22,6 +22,19 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def edit 
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update 
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def blog_post_params
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  patch "blog_posts/:id", to: "blog_posts#update"
+  get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   post "blog_posts", to: "blog_posts#create", as: :blog_posts
   # Defines the root path route ("/")
   root "blog_posts#index"
